b2da1f24b6685761b50b3c94db0e70eadf075fa6
amar-248 <amar.hallikhed@gmail.com>
2022-09-28 20:55:12 +0530

    First Commit with basic text

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hi This is a Demo Site
+      </header>
+    </div>
+  );
+}
+
+export default App;
